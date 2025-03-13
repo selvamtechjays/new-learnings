d65311bb4508a248072668cf39caa09e03b79f1b
selvamtechjays <selvam.murugaiah@techjays.com>
2025-03-13 22:11:05 +0530

    first commit

---
+++ app.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Simple Route
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, TypeScript Server is running!");
+});
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
